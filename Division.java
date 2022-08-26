1f86066cc34ebcec150a29a53ead12a6706215a0
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-08-26 15:30:52 +0530

    import java.util.Scanner; class First{     public static void main(String args[])     {         int a,b,c,s,d,e;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         // c=sc.nextDouble();         s=a/b;         // d=Math.sqrt(s*(s-a)*(s-b)*(s-c));         // e=Math.round(d*100)/100.0;         System.out.print(s);     } }

---
+++ Division.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class First{
+    public static void main(String args[])
+    {
+        int a,b,c,s,d,e;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        // c=sc.nextDouble();
+        s=a/b;
+        // d=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        // e=Math.round(d*100)/100.0;
+        System.out.print(s);
+    }
+}
