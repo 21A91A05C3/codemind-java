556a59f29edd0b703080f0347fd06396e89892e8
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-08-26 15:32:54 +0530

    import java.util.Scanner; class First{     public static void main(String args[])     {         int a,b,c,s,d,e;         Scanner sc=new Scanner(System.in);         a=sc.nextInt();         b=sc.nextInt();         s=a%b;         // d=Math.sqrt(s*(s-a)*(s-b)*(s-c));         // e=Math.round(d*100)/100.0;         System.out.print(s);     } }

---
+++ Modulus.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class First{
+    public static void main(String args[])
+    {
+        int a,b,c,s,d,e;
+        Scanner sc=new Scanner(System.in);
+        a=sc.nextInt();
+        b=sc.nextInt();
+        s=a%b;
+        // d=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        // e=Math.round(d*100)/100.0;
+        System.out.print(s);
+    }
+}
