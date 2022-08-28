23e010afc0d67005e2886da4fe4049c8576eb5df
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-08-28 09:46:46 +0530

    import java.util.Scanner; class Main{     public static void main(String args[])     {
            Scanner s=new Scanner(System.in);
            char n=s.next().charAt(0);
            if(n=='V'||n=='v')
            {
                System.out.print("Violet");
            }
            else if(n=='I'||n=='i')
            {
               System.out.print("Indigo"); 
            }
            else if(n=='B'||n=='b')
            {
               System.out.print("Blue"); 
            }
            else if(n=='G'||n=='g')
            {
               System.out.print("Green"); 
            }
            else if(n=='Y'||n=='y')
            {
               System.out.print("Yellow"); 
            }
            else if(n=='O'||n=='o')
            {
               System.out.print("Orange"); 
            }
            else if(n=='R'||n=='r')
            {
               System.out.print("Red"); 
            }
            else
            {
                System.out.print("-1");
            }
            
                    
        }
    }

---
+++ Color_Code.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[])
+    {
+        
+        Scanner s=new Scanner(System.in);
+        char n=s.next().charAt(0);
+        if(n=='V'||n=='v')
+        {
+            System.out.print("Violet");
+        }
+        else if(n=='I'||n=='i')
+        {
+           System.out.print("Indigo"); 
+        }
+        else if(n=='B'||n=='b')
+        {
+           System.out.print("Blue"); 
+        }
+        else if(n=='G'||n=='g')
+        {
+           System.out.print("Green"); 
+        }
+        else if(n=='Y'||n=='y')
+        {
+           System.out.print("Yellow"); 
+        }
+        else if(n=='O'||n=='o')
+        {
+           System.out.print("Orange"); 
+        }
+        else if(n=='R'||n=='r')
+        {
+           System.out.print("Red"); 
+        }
+        else
+        {
+            System.out.print("-1");
+        }
+        
+                
+    }
+}
