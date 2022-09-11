da9936ec3801b126ca7f232b8ea48030c559951e
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-09-11 14:35:04 +0530

    import java.util.Scanner; class Main{     public static void main(String args[]){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int []a=new int[n];         for(int i=0;i<n;i++){             a[i]=s.nextInt();         }         int d=0;         for(int i=0;i<n;i++){             d=d+a[i];         }         int k=d/n;         int c=0;         for(int i=0;i<n;i++){             if(a[i]>=k){                 c++;             }         }         System.out.print(c);     } }

---
+++ elements_greater_than_average.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+class Main{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []a=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        int d=0;
+        for(int i=0;i<n;i++){
+            d=d+a[i];
+        }
+        int k=d/n;
+        int c=0;
+        for(int i=0;i<n;i++){
+            if(a[i]>=k){
+                c++;
+            }
+        }
+        System.out.print(c);
+    }
+}
