b33429fafe933e0169c4432febfcc267ae63146f
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-10-04 00:08:28 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int a=s.nextInt();         int b=s.nextInt();         int k=(n*a*b)/100;         System.out.print(k);
        }
    }

---
+++ SIMPLE_INTEREST.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=s.nextInt();
+        int b=s.nextInt();
+        int k=(n*a*b)/100;
+        System.out.print(k);
+        
+        
+    }
+}
