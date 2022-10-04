2cac9fd00f5510efe3ff43f4d0204a1056cadc45
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-10-04 12:23:56 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int []l=new int[n];         for(int i=0;i<l.length;i++){             l[i]=s.nextInt();         }         int max=l[0];         for(int i=1;i<l.length;i++){             if(l[i]<max){                 max=l[i];             }         }         System.out.print(max);     }
    }

---
+++ minimum_element_of_an_array.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []l=new int[n];
+        for(int i=0;i<l.length;i++){
+            l[i]=s.nextInt();
+        }
+        int max=l[0];
+        for(int i=1;i<l.length;i++){
+            if(l[i]<max){
+                max=l[i];
+            }
+        }
+        System.out.print(max);
+    }
+    
+}
