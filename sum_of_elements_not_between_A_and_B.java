923c4e911f17ddad81388f8daf5191f11c262bbd
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-10-04 12:13:29 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int []l=new int[n];         for(int i=0;i<l.length;i++){             l[i]=s.nextInt();         }         int c=0,d=0;         int a=s.nextInt();         int b=s.nextInt();         for(int i=0;i<l.length;i++){             d+=l[i];         }         for(int i=0;i<l.length;i++){             if(l[i]>=a && l[i]<=b){                 c+=l[i];             }         }         System.out.print((d-c));     }
    }

---
+++ sum_of_elements_not_between_A_and_B.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []l=new int[n];
+        for(int i=0;i<l.length;i++){
+            l[i]=s.nextInt();
+        }
+        int c=0,d=0;
+        int a=s.nextInt();
+        int b=s.nextInt();
+        for(int i=0;i<l.length;i++){
+            d+=l[i];
+        }
+        for(int i=0;i<l.length;i++){
+            if(l[i]>=a && l[i]<=b){
+                c+=l[i];
+            }
+        }
+        System.out.print((d-c));
+    }
+    
+}
