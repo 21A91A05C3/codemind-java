7cf1ef87220a8fd06d6058856923b3670afbf038
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-10-04 12:32:47 +0530

    import java.util.*; class Main{     public static void main(String[] args){         Scanner s=new Scanner(System.in);         int c=s.nextInt();         float k=(float)(c*9)/5+32;         System.out.printf("%.2f",k);     }
    }

---
+++ Celsius_To_Fahrenheit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main{
+    public static void main(String[] args){
+        Scanner s=new Scanner(System.in);
+        int c=s.nextInt();
+        float k=(float)(c*9)/5+32;
+        System.out.printf("%.2f",k);
+    }
+    
+}
