af5547f2feba6914f8f2940781ebade434219d9f
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-11-04 23:47:08 +0530

    import java.util.*; class Main{     public static void main(String[]args){         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int []a=new int[n];         int []b=new int[n];         for(int i=0;i<n;i++){             a[i]=s.nextInt();         }         for(int i=0;i<n;i++){             int c=0;             for(int j=0;j<n;j++){                 if(a[j]<a[i])c++;             }             b[i]=c;         }          for(int i=0;i<n;i++){             System.out.print(b[i]+" ");         }
        }
    }

---
+++ How_Many_Numbers_Are_Smaller_Than_the_Current_Number.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class Main{
+    public static void main(String[]args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int []a=new int[n];
+        int []b=new int[n];
+        for(int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        for(int i=0;i<n;i++){
+            int c=0;
+            for(int j=0;j<n;j++){
+                if(a[j]<a[i])c++;
+            }
+            b[i]=c;
+        }
+         for(int i=0;i<n;i++){
+            System.out.print(b[i]+" ");
+        }
+        
+    }
+}
