620175c0b2d20fc54b74812081a006b7f69c4448
SAI GANESH <97424745+21A91A05C3@users.noreply.github.com>
2022-11-27 11:59:31 +0530

    import java.util.*; class S {     public static void main(String args[])     {         Scanner s=new Scanner(System.in);         int n=s.nextInt();         int k=n*(n-3)/2;         System.out.print(k);     } }

---
+++ Diagonals_of_polygon.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class S
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int k=n*(n-3)/2;
+        System.out.print(k);
+    }
+}
